852fd207310a1d80b866ae56513573d471630893
Jefferson Miranda <jefferson472@hotmail.com>
2023-05-13 07:35:14 -0300

    feat: add model produto

---
+++ Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace quitanda_online.Models
+{
+    public class Product
+    {
+        [Key]
+        public int IdProduct { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Column(TypeName = "decimanl(18, 2)")]
+        public string Price { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public int Inventory { get; set; }
+
+    }
+}
