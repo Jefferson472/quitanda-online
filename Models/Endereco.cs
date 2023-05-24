393c1fa4056eabce2d1d0bd9e0311ab6f4cc42b9
Jefferson Miranda <jefferson472@hotmail.com>
2023-05-24 07:49:48 -0300

    feat: add novo modelo Endereço

---
+++ Models/Endereco.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace quitanda_online.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O CEP informado não retornou um endereço válido.")]
+        [MaxLength(100, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [MaxLength(10, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        [Display(Name = "Número")]
+        public string Numero { get; set; }
+
+        [MaxLength(100, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [MaxLength(2, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [RegularExpression(@"[0-9]{8}$", ErrorMessage = "O campo {0} deve ser preenchido com um CEP válido.")]
+        [MaxLength(11, ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [UIHint("_CepTemplate")]
+        public string CEP { get; set; }
+
+        [MaxLength(100, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        [Display(Name = "Referência")]
+        public string Referencia { get; set; }
+    }
+}
---
+++ Pages/Shared/DisplayTemplates/_CepTemplate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace quitanda_online.Pages.Shared.DisplayTemplates
+{
+    public class _CepTemplate : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
