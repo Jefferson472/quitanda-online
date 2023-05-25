60db1aa7856d9c95cf16d2f48623e1984ff0feb9
Jefferson Miranda <jefferson472@hotmail.com>
2023-05-25 19:51:48 -0300

    fix: add datanotation para Id

---
+++ Models/Pedido.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace quitanda_online.Models
+{
+    public class Pedido
+    {
+        public enum SituacaoPedido
+        {
+            Cancelado,
+            Realizado,
+            Verificado,
+            Atendido,
+            Entregue,
+        }
+
+        [Key]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [Display(Name = "Data/Hora")]
+        public DateTime DataHoraPedido { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Display(Name = "Valor Total")]
+        public decimal ValorTotal { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
+        [DisplayName("Situação")]
+        public SituacaoPedido Situacao { get; set; }
+
+        public int IdCliente { get; set; }
+
+        [ForeignKey("IdCliente")]
+        public Cliente Cliente { get; }
+
+        public Endereco Endereco { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
